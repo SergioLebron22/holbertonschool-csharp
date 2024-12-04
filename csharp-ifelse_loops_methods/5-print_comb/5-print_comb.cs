fc24cbae9faf3c812487ac2fabc2dbcd2a146f01
SergioLebron22 <lebronsergio22@gmail.com>
2024-12-03 21:07:14 -0400

    feat: add program to print numbers from 0 to 99 with project files

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 99; i++)
+        {
+            Console.Write(i < 99 ? $"{i:D2}, " : $"{i:D2}");
+        }
+        Console.Write("\n");
+    }
+}

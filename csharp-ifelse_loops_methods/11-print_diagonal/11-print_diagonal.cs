de6dc0a068ef1ea1ac2d07fd7e45de843206c6d8
SergioLebron1 <sergio.lebron1@upr.edu>
2024-12-04 11:31:28 -0400

    feat: add PrintDiagonal method to print a diagonal line based on specified length with project files

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Line {
+    public static void PrintDiagonal(int length){
+        int i;
+        int c;
+
+        c = 0;
+
+        while (length > 0) {
+            i = c;
+            while (i > 0) {
+                Console.Write(" ");
+                i--;
+            }
+            Console.Write("\\");
+            Console.Write("\n");
+            c++;
+            length--;
+        }
+        if (c < 1) {
+            Console.Write("\n");
+        }
+    }
+}

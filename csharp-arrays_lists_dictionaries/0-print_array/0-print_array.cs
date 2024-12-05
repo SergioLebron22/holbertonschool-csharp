2c131d4a505f8abc2bc3a0f9a7d9ab3437d2d65d
SergioLebron22 <lebronsergio22@gmail.com>
2024-12-05 14:19:18 -0400

    fix: add validation for negative size in CreatePrint method

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -2,15 +2,16 @@
 
 class Array {
     public static int[] CreatePrint(int size) {
+        if (size < 0){
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
         int[] array = new int[size];
 
         if (size == 0) {
             Console.WriteLine();
             return array;
-        }
-        if (size < 0){
-            Console.WriteLine("Size cannot be negative");
-            return null;
         }
 
         for (int i = 0; i < size; i++) {

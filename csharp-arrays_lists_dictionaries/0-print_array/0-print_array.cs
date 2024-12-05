c77d6573a053e5a27e9c846f09479b9d9ae34729
SergioLebron22 <lebronsergio22@gmail.com>
2024-12-05 14:33:01 -0400

    fix: improve array creation and printing logic in CreatePrint method

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,23 +1,35 @@
 ﻿using System;
 
-class Array {
-    public static int[] CreatePrint(int size) {
-        if (size < 0){
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
             Console.WriteLine("Size cannot be negative");
             return null;
         }
-        else if (size == 0) {
+        else if (size == 0)
+        {
             Console.WriteLine();
             return new int[0];
         }
-        else {
-            int[] array = new int[size];
-            for (int i = 0; i < size; i++) {
-                array[i] = i;
-                Console.Write($"{i} ");
+        else
+        {
+            int[] newArray = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                newArray[i] = i;
+                if (i == size -1)
+                {
+                    Console.Write("{0}\n", i);
+                }
+                else
+                {
+                    Console.Write("{0} ", i);
+                }
             }
-            Console.WriteLine();
-            return array;
+            return newArray;
         }
     }
 }

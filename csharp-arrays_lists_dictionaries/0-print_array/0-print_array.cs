4f921845c5317e481f59d9cc4ebbbe9f89f74a78
SergioLebron22 <lebronsergio22@gmail.com>
2024-12-05 13:39:37 -0400

    feat: add program to create and print an array of specified size with project files

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Array {
+    public static int[] CreatePrint(int size) {
+        int[] array = new int[size];
+
+        if (size == 0) {
+            Console.WriteLine();
+            return array;
+        }
+        if (size < 0){
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        for (int i = 0; i < size; i++) {
+            array[i] = i;
+            Console.Write($"{i} ");
+        }
+        Console.WriteLine();
+        return array;
+    }
+}

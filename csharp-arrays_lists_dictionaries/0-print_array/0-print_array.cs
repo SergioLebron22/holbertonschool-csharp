9ebdd75845be85c0f478bc3edf8ee9693a55498c
SergioLebron22 <lebronsergio22@gmail.com>
2024-12-05 14:22:53 -0400

    fix: refactor array initialization and handling of zero size in PrintArray method

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -6,19 +6,18 @@ class Array {
             Console.WriteLine("Size cannot be negative");
             return null;
         }
-
-        int[] array = new int[size];
-
-        if (size == 0) {
+        else if (size == 0) {
+            Console.WriteLine();
+            return new int[0];
+        }
+        else {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++) {
+                array[i] = i;
+                Console.Write($"{i} ");
+            }
             Console.WriteLine();
             return array;
         }
-
-        for (int i = 0; i < size; i++) {
-            array[i] = i;
-            Console.Write($"{i} ");
-        }
-        Console.WriteLine();
-        return array;
     }
 }

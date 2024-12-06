24e33f7443ab0d80df60f5fc922f1c334c050096
SergioLebron22 <lebronsergio22@gmail.com>
2024-12-06 15:01:32 -0400

    feat: add CreatePrint method to generate and print a list of integers

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List {
+    public static List<int> CreatePrint(int size) {
+        if (size < 0) {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> newList = [];
+
+        for (int i = 0; i < size; i++) {
+            newList.Add(i);
+            Console.Write("{0} ", i);
+        }
+        Console.WriteLine();
+        return newList;
+    }
+}

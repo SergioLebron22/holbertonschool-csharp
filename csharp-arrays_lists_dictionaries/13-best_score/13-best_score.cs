89b0d4ef518af6ce5e6d3fecd131f31b30b42da9
SergioLebron22 <lebronsergio22@gmail.com>
2024-12-07 14:53:40 -0400

    feat: add BestScore method to find the highest score in a dictionary

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary {
+    public static string BestScore(Dictionary<string, int> myList) {
+        if (myList.Count == 0) {
+            return "None";
+        }
+
+        string result = "";
+        int topScore = 0;
+
+        foreach (KeyValuePair<string, int> kvp in myList) {
+            if (topScore <= kvp.Value) {
+                result = kvp.Key;
+                topScore = kvp.Value;
+            }
+        }
+        return result;
+    }
+}

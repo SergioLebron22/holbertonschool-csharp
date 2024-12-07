bc3d25f6d02d7fc7663e8521810af9f970b45755
SergioLebron22 <lebronsergio22@gmail.com>
2024-12-07 13:24:02 -0400

    feat: add MultiplyBy2 method to double the values in a dictionary

---
+++ csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary {
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict) {
+
+        Dictionary<string, int> newDict = new Dictionary<string, int>();
+
+        foreach (KeyValuePair<string, int> kvp in myDict) {
+            int newValue = kvp.Value * 2;
+            newDict.Add(kvp.Key, newValue);
+        }
+
+        return newDict;
+    }
+}

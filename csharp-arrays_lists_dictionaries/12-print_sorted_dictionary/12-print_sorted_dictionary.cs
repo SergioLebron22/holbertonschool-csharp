beb5fe0c8a2eb5325dca8a48b3bce3d6874aa3e0
SergioLebron22 <lebronsergio22@gmail.com>
2024-12-07 14:01:28 -0400

    feat: add PrintSorted method to output a sorted dictionary by keys

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary {
+    public static void PrintSorted(Dictionary<string, string> myDict) {
+        var sortedByKey = myDict.OrderBy(kvp => kvp.Key);
+
+        foreach (var kvp in sortedByKey) {
+            Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+        }
+    }
+}

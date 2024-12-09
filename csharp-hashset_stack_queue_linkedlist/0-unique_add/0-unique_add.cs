49d31c29f5285791ed7d665cccfa3854e1ae4340
SergioLebron1 <sergio.lebron1@upr.edu>
2024-12-09 13:19:21 -0400

    feat: implement Sum method to calculate the sum of unique integers in a list

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List {
+    public static int Sum(List<int> myList) {
+        HashSet<int> uniqNums = new HashSet<int>();
+
+        for (int i = 0; i < myList.Count(); i++) {
+            uniqNums.Add(myList[i]);
+        }
+        
+        int sum = 0;
+        foreach (int num in uniqNums) {
+            sum += num;
+        }
+
+        return sum;
+    }
+}

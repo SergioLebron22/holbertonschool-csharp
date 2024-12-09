4ebd5f77af2564c8f3a6096c4050297640ee214a
SergioLebron1 <sergio.lebron1@upr.edu>
2024-12-09 14:13:03 -0400

    refactor: optimize Sum method by initializing HashSet with input list

--- csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -3,12 +3,8 @@ using System.Collections.Generic;
 
 class List {
     public static int Sum(List<int> myList) {
-        HashSet<int> uniqNums = new HashSet<int>();
+        HashSet<int> uniqNums = new HashSet<int>(myList);
 
-        for (int i = 0; i < myList.Count(); i++) {
-            uniqNums.Add(myList[i]);
-        }
-        
         int sum = 0;
         foreach (int num in uniqNums) {
             sum += num;

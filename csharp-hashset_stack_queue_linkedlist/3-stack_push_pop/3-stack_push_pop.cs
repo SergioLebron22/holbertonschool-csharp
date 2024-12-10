68bbdbbc710925e8dd28262f7d6b9d28ab806569
SergioLebron22 <lebronsergio22@gmail.com>
2024-12-09 22:06:32 -0400

    feat: add MyStack class with Info method for stack operations

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack {
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search){
+        Console.WriteLine($"Number of items: {aStack.Count}");
+
+        if (aStack.Count != 0) {
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        }
+        else {
+            Console.WriteLine("Stack is empty");
+        }
+
+        if (aStack.Contains(search)) {
+            Console.WriteLine($"Stack contains {search}: {aStack.Contains(search)}");
+
+            while (aStack.Contains(search)) {
+                aStack.Pop();
+
+            }
+        }
+
+        if (aStack.Contains(search) == false) {
+            aStack.Push(newItem);
+        }
+
+        return aStack;
+
+    }
+}

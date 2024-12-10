2d00f0ea78d72acb9f6906556c4aaf8917b2eb8a
SergioLebron1 <sergio.lebron1@upr.edu>
2024-12-10 12:01:56 -0400

    fix: improve MyStack Info method to handle empty stack and streamline search logic

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -5,27 +5,19 @@ class MyStack {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search){
         Console.WriteLine($"Number of items: {aStack.Count}");
 
-        if (aStack.Count != 0) {
-            Console.WriteLine($"Top item: {aStack.Peek()}");
-        }
-        else {
+        if (aStack.Count == 0) {
             Console.WriteLine("Stack is empty");
         }
+        else {
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        }
 
+        Console.WriteLine($"Stack contains \"{search}\": {aStack.Contains(search)}");
         if (aStack.Contains(search)) {
-            Console.WriteLine($"Stack contains \"{search}\": {aStack.Contains(search)}");
-
-            while (aStack.Contains(search)) {
-                aStack.Pop();
-
-            }
+            while (aStack.Pop() != search); 
         }
-
-        if (aStack.Contains(search) == false) {
-            aStack.Push(newItem);
-        }
-
+        
+        aStack.Push(newItem);
         return aStack;
-
     }
 }

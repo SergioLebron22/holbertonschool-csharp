95ae1feca4b93d19850a717c0390a51913ba9077
SergioLebron22 <lebronsergio22@gmail.com>
2024-12-11 13:53:03 -0400

    feat: add FindNode method to LList class for locating a node in a linked list

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static int FindNode(LinkedList<int> myLList, int value) {
+        int idx = 0;
+        LinkedListNode<int> current = myLList.First;
+
+        while (current.Value != value) {
+            idx++;
+            current = current.Next;
+
+            if (current.Next == null) {
+                return -1;
+            }
+        }
+
+        return idx;
+    }
+}

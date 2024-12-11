626c690ed1d3f9ba78b71918c259beab839f892b
SergioLebron22 <lebronsergio22@gmail.com>
2024-12-11 18:08:18 -0400

    feat: add Pop method to LList class for removing and returning the head node from a linked list

---
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static int Pop(LinkedList<int> myLList) {
+        LinkedListNode<int> head = myLList.First;
+        myLList.RemoveFirst();
+        return head.Value;
+    }
+}

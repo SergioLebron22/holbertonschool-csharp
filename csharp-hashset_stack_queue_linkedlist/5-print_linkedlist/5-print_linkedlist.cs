9ae69b021cae796de8804b8c9931678564e1f13f
SergioLebron22 <lebronsergio22@gmail.com>
2024-12-10 21:47:43 -0400

    feat: add LList class with CreatePrint method to generate and print a linked list

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static LinkedList<int> CreatePrint(int size) {
+        if (size < 0) {
+            return new LinkedList<int>();
+        }
+
+        LinkedList<int> ll = new LinkedList<int>();
+
+        for (int i=0; i < size; i++) {
+            ll.AddLast(i);
+            Console.WriteLine(i);
+        }
+
+        return ll;
+    }
+}

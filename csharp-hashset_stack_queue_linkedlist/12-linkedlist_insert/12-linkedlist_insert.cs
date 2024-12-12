7feff0eaeee72db03e469e19525f0add46259ac1
SergioLebron22 <lebronsergio22@gmail.com>
2024-12-11 22:28:13 -0400

    feat: add Insert method to LList class for inserting elements in sorted order

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n) {
+        LinkedListNode<int> current = myLList.First;
+
+        if (myLList.First == null) {
+            return myLList.AddFirst(n);
+        }
+
+        while (current != null) {
+            if (current.Value > n) {
+                return myLList.AddBefore(current, n);
+            }
+            current = current.Next;
+        }
+        return myLList.AddLast(n);
+    }
+}

2796fae0f8b6d850375171981e5968c71be05cd0
SergioLebron22 <lebronsergio22@gmail.com>
2024-12-11 21:24:24 -0400

    feat: add GetNode method to LList class for retrieving a node's value by index

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static int GetNode(LinkedList<int> myLList, int n) {
+        if (n < 0 || n >= myLList.Count) [
+            return 0;
+        ]
+
+        LinkedListNode<int> current = myLList.First;
+        for (int i=0; i < n; i++) {
+            current = current.Next;
+        }
+        return current.Value;
+    }
+}

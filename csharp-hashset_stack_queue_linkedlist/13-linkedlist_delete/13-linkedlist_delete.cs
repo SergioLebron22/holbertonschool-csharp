e30019089471b5c8048d6a9bae710f80b0fdd343
SergioLebron22 <lebronsergio22@gmail.com>
2024-12-11 22:32:56 -0400

    feat: add Delete method to LList class for removing an element by index

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index) {
+        if (index < 0 || index > myLList.Count)
+            return;
+
+        LinkedListNode<int> current = myLList.First;
+        int indexCount = 0;
+        
+        while (current != null) {
+            if (indexCount == index) {
+                myLList.Remove(current);
+            }
+            current = current.Next;
+            indexCount++;
+        }
+    }
+}

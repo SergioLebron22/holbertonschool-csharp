059d068c493f3210d57e3fc9e9f8dc753e4551cf
SergioLebron22 <lebronsergio22@gmail.com>
2025-01-08 16:48:43 -0400

    feat: add XML documentation for Zombie constructor

--- csharp-classes/2-enemy/2-enemy.cs
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -16,7 +16,11 @@ namespace Enemies {
         public Zombie() {
             health = 0;
         }
-
+        /// <summary>
+        /// Constructor for zombie class
+        /// </summary>
+        /// <param name="value"></param>
+        /// <exception cref="ArgumentException"></exception>
         public Zombie(int value) {
             if (value < 0) throw new ArgumentException("Health must be greater or equal to 0");
             health = value;

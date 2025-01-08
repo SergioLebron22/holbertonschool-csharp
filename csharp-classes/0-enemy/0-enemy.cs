0bd595768dea20ad6848127c7bbe07f0bd838f95
SergioLebron22 <lebronsergio22@gmail.com>
2025-01-08 14:49:26 -0400

    feat: add Zombie class and project file for Enemies namespace

---
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>
+/// The Enemies namespace contains classes related to enemy characters.
+/// </summary>
+namespace Enemies {
+    /// <summary>
+    /// Represents a Zombie enemy character.
+    /// </summary>
+    public class Zombie {
+
+    }
+}

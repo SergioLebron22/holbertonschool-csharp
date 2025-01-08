65ca90e89dcf8d35c4fe685f4acbc216643deea1
SergioLebron22 <lebronsergio22@gmail.com>
2025-01-08 16:22:01 -0400

    refactor: remove redundant XML documentation from Enemy class

--- csharp-classes/0-enemy/0-enemy.cs
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 
-/// <summary>
-/// The Enemies namespace contains classes related to enemy characters.
-/// </summary>
 namespace Enemies {
     /// <summary>
     /// Represents a Zombie enemy character.

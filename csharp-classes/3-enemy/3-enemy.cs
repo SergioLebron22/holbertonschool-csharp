2e10b6ff6daf2ce62f98642001d544ba9ea42d27
SergioLebron22 <lebronsergio22@gmail.com>
2025-01-09 12:16:06 -0400

    feat: add Zombie class and project file for Enemies namespace

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Enemies {
+    /// <summary>
+    /// Represents a Zombie enemy character.
+    /// </summary>
+    public class Zombie {
+        /// <summary>
+        /// Represents the health of the zombie
+        /// </summary>
+        private int health;
+
+        /// <summary>
+        /// Constructor for zombie class
+        /// </summary>
+        public Zombie() {
+            this.health = 0;
+        }
+
+        /// <summary>
+        /// Constructor for zombie class
+        /// </summary>
+        public Zombie(int value) {
+            if (value >= 0) this.health = value;
+            else throw new ArgumentException("Health must be greater than or equal to 0");
+        }
+        /// <summary>
+        /// Getter for zombie health property
+        /// </summary>
+        public int GetHealth() {
+            return this.health;
+        }
+    }
+}

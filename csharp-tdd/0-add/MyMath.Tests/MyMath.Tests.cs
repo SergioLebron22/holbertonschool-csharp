6ea3fcf9b48e7ee5e2a2efedf57312541b1c96ea
SergioLebron22 <lebronsergio22@gmail.com>
2025-01-11 13:05:49 -0400

    feat: add MyMath project with addition operations and corresponding tests

---
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,46 @@
+using MyMath;
+using NUnit.Framework;
+
+namespace MyMath.Tests{
+    [TestFixture]
+    public class Tests
+    {
+        /// <summary>
+        /// Test sum of two positive numbers
+        /// </summary>
+        [Test]
+        public void Sum2()
+        {
+            Assert.That(Operations.Add(4, 7), Is.EqualTo(11));
+        }
+
+        /// <summary>
+        /// test the sum of a min and max value
+        /// </summary>
+        [Test]
+        public void SumMinMax()
+        {
+            int min = int.MinValue;
+            int max = int.MaxValue;
+            Assert.That(Operations.Add(min, max), Is.EqualTo(min + max));
+        }
+
+        /// <summary>
+        /// test the sum of a negative number 
+        /// </summary>
+        [Test]
+        public void SumNegative()
+        {
+            Assert.That(Operations.Add(10, -3), Is.EqualTo(7));
+        }
+
+        /// <summary>
+        /// test the sum of zeros
+        /// </summary>
+        [Test]
+        public void SumZero()
+        {
+            Assert.That(Operations.Add(0, 0), Is.EqualTo(0));
+        }
+    }
+}
---
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyMath {
+    /// <summary>
+    /// Operations class
+    /// </summary>
+    public class Operations
+    {
+        /// <summary>
+        /// Method for adding two integer values
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>the sum of two integers</returns>
+        public static int Add(int a, int b) {
+            return a + b;
+        }
+    }
+
+}
+

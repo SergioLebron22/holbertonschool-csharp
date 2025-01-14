021fb5f92d3a7f54513b75481e10529d0a9132ed
SergioLebron22 <lebronsergio22@gmail.com>
2025-01-14 19:09:37 -0400

    feat: add Obj class with Print method and project files for C# type reflection example

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+class Obj {
+    public static void Print(object obj) {
+        Type type = obj.GetType();
+
+        Console.WriteLine($"{type.Name} Properties:");
+        foreach (PropertyInfo property in type.GetProperties()) {
+            Console.WriteLine(property.Name);
+        }
+        Console.WriteLine($"{type.Name} Methods:");
+        foreach (MethodInfo method in type.GetMethods()) {
+            Console.WriteLine(method.Name);
+        }
+    }
+}

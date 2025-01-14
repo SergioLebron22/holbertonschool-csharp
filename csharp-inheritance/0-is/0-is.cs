8c1f9205abf1858fff9ad3d2d75c7ba9c90df011
SergioLebron22 <lebronsergio22@gmail.com>
2025-01-14 16:21:04 -0400

    docs: add XML documentation for IsOfTypeInt method in Obj class

--- csharp-inheritance/0-is/0-is.cs
+++ csharp-inheritance/0-is/0-is.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 
 class Obj {
+    /// <summary>
+    /// A method that checks if the object passed is int
+    /// </summary>
+    /// <returns>true if obj is int, else false</returns>
     public static bool IsOfTypeInt(object obj) {
         if (obj is int) return true;
         return false;

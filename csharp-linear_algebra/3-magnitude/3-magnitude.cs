114baf43c82603789568690c014c70194f51a754
SergioLebron22 <lebronsergio22@gmail.com>
2025-01-29 13:09:37 -0400

    feat: implement magnitude calculation for 2D and 3D vectors

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class VectorMath {
+    public static double Magnitude(double[] vector) {
+        if (vector.Length != 2 && vector.Length != 3)
+            return -1;
+
+        double power = 0;
+        foreach (double value in vector) {
+            power += Math.Pow(value, 2);
+        }
+
+        double result = Math.Sqrt(power);
+
+        return Math.Round(result, 2);
+    }
+}

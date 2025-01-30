9404e380043aa971cfb3231d5a8b1cb05ef9f232
SergioLebron22 <lebronsergio22@gmail.com>
2025-01-30 17:07:41 -0400

    feat: implement vector scalar multiplication for 2D and 3D vectors

---
+++ csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class VectorMath {
+    public static double[] Multiply(double[] vector, double scalar) {
+        if (vector.Length != 2 && vector.Length != 3) return new double[] { -1 };
+
+        for (int i=0; i < vector.Length; i++) {
+            vector[i] *= scalar;
+        }
+
+        return vector;
+    }
+}

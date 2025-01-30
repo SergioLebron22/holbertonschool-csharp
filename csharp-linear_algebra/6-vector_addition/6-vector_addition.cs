21504e11706901c088b57b38f4f5cb056ded5dfa
SergioLebron22 <lebronsergio22@gmail.com>
2025-01-30 15:37:58 -0400

    fix: correct vector addition method to properly validate input lengths and return the correct result

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -3,16 +3,15 @@
 class VectorMath {
     public static double[] Add(double[] vector1, double[] vector2) {
         
-        if (vector1.Length == 2 && vector2 == 2 || vector1.Length == 3 && vector2.Length == 3){
-            double[] resultVector = new double[vector1.Length];
+        if (vector1.Length == 2 && vector2.Length == 2 || vector1.Length == 3 && vector2.Length == 3){
 
             for (int i=0; i < vector1.Length; i++) {
-                resultVector[i] = vector1[i] + vector2[i];
+                vector1[i] += vector2[i];
             }
 
-            return resultVector;
+            return vector1;
         }
 
-        return new double[] {-1};
+        return new double[] { -1 };
     }
 }

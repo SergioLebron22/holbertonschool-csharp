e46449f1854829475a9a318e33e2f82578de376b
SergioLebron22 <lebronsergio22@gmail.com>
2025-01-30 18:28:00 -0400

    feat: implement dot product calculation for 2D and 3D vectors

---
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class VectorMath {
+    public static double DotProduct(double[] vector1, double[] vector2) {
+        if (vector1.Length == 2 && vector2.Length == 2 || vector1.Length == 3 && vector2.Length == 3) {
+
+            double result = 0;
+            for(int i=0; i < vector1.Length; i++){
+                result += vector1[i] * vector2[i];
+            }
+            return result;
+        }
+    }
+}

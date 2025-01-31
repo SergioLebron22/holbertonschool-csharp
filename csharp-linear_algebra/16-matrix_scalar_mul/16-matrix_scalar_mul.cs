c5654a81afb0dfac808057fda0e12ba453de6575
SergioLebron22 <lebronsergio22@gmail.com>
2025-01-30 20:58:34 -0400

    feat: implement matrix scalar multiplication functionality

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MatrixMath {
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar) {
+        if (matrix.Rank != 2 && matrix.Rank != 3) return new double[,] {{-1}};
+
+        int cols = matrix.GetLength(0);
+        int rows = matrix.GetLength(1);
+
+        double[,] result = new double[cols, rows];
+
+        for (int i=0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                result[i,j] *= scalar;
+            }
+        }
+
+        return result;
+    }
+}

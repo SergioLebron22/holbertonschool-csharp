089c8d61be88245c6a474cc73d3bdb4e3e79e319
SergioLebron22 <lebronsergio22@gmail.com>
2025-01-30 20:46:09 -0400

    feat: implement matrix addition functionality

---
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class MatrixMath {
+    public static double[,] Add(double[,] matrix1, double[,] matrix2) {
+        if (((matrix1.GetLength(0) == 2 && matrix1.GetLength(1) == 2) ||
+            (matrix1.GetLength(0) == 3 && matrix1.GetLength(1) == 3)) &&
+            matrix1.GetLength(0) == matrix2.GetLength(0))
+        {
+            int cols = matrix1.GetLength(0);
+            int rows = matrix1.GetLength(1);
+            double[,] resultMatrix = new double[cols,rows];
+
+            for (int i = 0; i < cols; i++){
+                for(int j = 0; j < rows; j++) {
+                    resultMatrix[i,j] = matrix1[i,j] + matrix2[i,j];
+                }
+            }
+            return resultMatrix;
+        }
+        return new double[,] {{-1}};
+    }
+}

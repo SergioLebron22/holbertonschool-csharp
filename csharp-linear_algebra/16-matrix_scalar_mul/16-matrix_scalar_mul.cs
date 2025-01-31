f3db7b0fd527339bb77a13d509d971cfcdb6c338
SergioLebron22 <lebronsergio22@gmail.com>
2025-01-30 21:02:59 -0400

    fix: update scalar multiplication validation logic in matrix multiplication

--- csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -2,19 +2,21 @@
 
 class MatrixMath {
     public static double[,] MultiplyScalar(double[,] matrix, double scalar) {
-        if (matrix.GetLength(0) != matrix.GetLength(1) && (matrix.GetLength(0) != 2 || matrix.GetLength(0) != 3) ) return new double[,] {{-1}};
+        if (matrix.GetLength(0) == matrix.GetLength(1) && (matrix.GetLength(0) == 2 || matrix.GetLength(0) == 3) ) {
 
-        int cols = matrix.GetLength(0);
-        int rows = matrix.GetLength(1);
+            int cols = matrix.GetLength(0);
+            int rows = matrix.GetLength(1);
 
-        double[,] result = new double[cols, rows];
+            double[,] result = new double[cols, rows];
 
-        for (int i=0; i < cols; i++) {
-            for (int j = 0; j < rows; j++) {
-                result[i,j] *= scalar;
+            for (int i=0; i < cols; i++) {
+                for (int j = 0; j < rows; j++) {
+                    result[i,j] *= scalar;
+                }
             }
-        }
 
-        return result;
+            return result;
+        }
+        return new double[,] {{-1}};
     }
 }

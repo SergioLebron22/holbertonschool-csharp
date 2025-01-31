78bc406ba9f5caf073376d358ab659c20c9618f2
SergioLebron22 <lebronsergio22@gmail.com>
2025-01-31 19:33:53 -0400

    feat: add 2D matrix rotation functionality with MatrixMath class

---
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+class MatrixMath
+{
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        double sin = Math.Sin(angle);
+        double cos = Math.Cos(angle);
+        double sum;
+        double[,] result = new double[rows, cols];
+
+        if (rows != 2 || cols != 2)
+            return new double[,] { { -1 } };
+
+        double[,] SinCosMatrix = { { cos, sin }, { -sin, cos } };
+
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                sum = 0;
+                for (int i = 0; i < cols; i++)
+                {
+                    sum += matrix[row, i] * SinCosMatrix[i, col];
+                }
+                result[row, col] = Math.Round(sum, 2);
+            }
+        }
+        return result;
+    }
+}

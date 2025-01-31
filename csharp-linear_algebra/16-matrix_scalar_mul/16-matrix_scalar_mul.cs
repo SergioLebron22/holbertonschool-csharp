c0108141479e64eaef485e73edc8976859651684
SergioLebron22 <lebronsergio22@gmail.com>
2025-01-31 17:15:13 -0400

    refactor: enhance MultiplyScalar method with XML documentation and fix result handling

--- csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -1,21 +1,24 @@
 ﻿using System;
 
-class MatrixMath {
-    public static double[,] MultiplyScalar(double[,] matrix, double scalar) {
-        if (matrix.GetLength(0) == matrix.GetLength(1) && (matrix.GetLength(0) == 2 || matrix.GetLength(0) == 3) ) {
-
-            int cols = matrix.GetLength(0);
-            int rows = matrix.GetLength(1);
-
-            double[,] result = new double[cols, rows];
-
-            for (int i=0; i < cols; i++) {
-                for (int j = 0; j < rows; j++) {
-                    result[i,j] *= scalar;
+/// <summary>Contains methods to perform matrix operations</summary>
+class MatrixMath
+{
+    /// <summary>Multiplies a matrix and a scalar</summary>
+    /// <param name="matrix">Matrix to multiply</param>
+    /// <param name="scalar">Scalar to multiply</param>
+    /// <returns>Resulting matrix</returns>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        if (matrix.GetLength(0) == matrix.GetLength(1) && (matrix.GetLength(0) == 2 || matrix.GetLength(0) == 3))
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] *= scalar;
                 }
             }
-
-            return result;
+            return matrix;
         }
         return new double[,] {{-1}};
     }

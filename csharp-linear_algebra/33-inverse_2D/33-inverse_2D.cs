790e465e7c35ed542c64eb96b6379fc2b248083a
SergioLebron22 <lebronsergio22@gmail.com>
2025-02-01 20:54:53 -0400

    feat: implement 2D matrix inverse calculation

---
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+public class MatrixMath
+{
+
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        {
+            return new double[,] { { -1 } };
+        }
+
+        double a = matrix[0, 0];
+        double b = matrix[0, 1];
+        double c = matrix[1, 0];
+        double d = matrix[1, 1];
+
+        double det = (a * d) - (b * c);
+
+        if (det == 0)
+        {
+            return new double[,] { { -1 } };
+        }
+
+        double[,] inverse = new double[2, 2];
+
+        inverse[0, 0] = Math.Round(d / det, 2);
+        inverse[0, 1] = Math.Round(-b / det, 2);
+        inverse[1, 0] = Math.Round(-c / det, 2);
+        inverse[1, 1] = Math.Round(a / det, 2);
+
+        return inverse;
+    }
+}

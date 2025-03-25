af04b952bc0505910a0f5aa39859170240ba8bf1
SergioLebron22 <lebronsergio22@gmail.com>
2025-03-25 18:27:27 -0400

    feat: implement generic Queue class with Enqueue method and type checking

---
+++ csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+public class Queue<T> 
+{
+
+    public Type CheckType() {
+        return typeof(T);
+    } 
+
+    public class Node {
+
+        public T value;
+        public Node? next = null;
+
+        public Node(T value) {
+            this.value = value;
+        }
+    }
+
+    public Node? head;
+    public Node? tail;
+    public int count = 0;
+
+    public void Enqueue(T value) {
+        Node newNode = new Node(value);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail!.next = newNode;
+            tail = newNode;
+        }
+
+        count++;
+    }
+
+    public int Count() {
+        return count;
+    }
+
+
+}

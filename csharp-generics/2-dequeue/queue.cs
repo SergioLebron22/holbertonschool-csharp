d0a9adb787922cd36fb70b25faccc2cf1800cf75
SergioLebron22 <lebronsergio22@gmail.com>
2025-03-25 19:38:40 -0400

    feat: add Queue class with Enqueue and Dequeue methods for generic type handling

---
+++ csharp-generics/2-dequeue/queue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+public class Queue<T> 
+{
+    public Type CheckType() {
+        return typeof(T);
+    } 
+
+    public class Node {
+
+        public T value;
+        public Node next = null;
+
+        public Node(T value) {
+            this.value = value;
+        }
+    }
+
+    public Node head;
+    public Node tail;
+    public int count = 0;
+
+    public void Enqueue(T value) {
+        Node newNode = new Node(value);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        count++;
+    }
+
+    public T Dequeue() {
+        if (head == null) {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        else {
+            T value = head.value;
+            head = head.next;
+            count--;
+            if (head == null) {
+                tail = null;
+            }
+            return value;
+        }
+    }
+
+    public int Count() {
+        return count;
+    }
+}

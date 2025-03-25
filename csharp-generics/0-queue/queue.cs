9916098b89f0b750db7febb19d61f02581cf5411
SergioLebron22 <lebronsergio22@gmail.com>
2025-03-25 16:48:34 -0400

    feat: add XML documentation for generic Queue class and CheckType method

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -1,7 +1,15 @@
 ﻿using System;
 
+/// <summary>
+/// This is a queue class with generic type
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public class Queue<T> 
 {
+    /// <summary>
+    /// returns the type of the queue
+    /// </summary>
+    /// <returns>type of T</returns>
     public Type CheckType() {
         return typeof(T);
     }    

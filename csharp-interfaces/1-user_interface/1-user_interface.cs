63513b8450ce72667ca20ac441cb606596b27fbb
SergioLebron22 <lebronsergio22@gmail.com>
2025-03-26 11:27:41 -0400

    feat: add Base class and interfaces for interactive, breakable, and collectable objects

---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+abstract class Base {
+    public string name;
+
+    public override string ToString() {
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
+
+interface IInteractive {
+    void Interact();
+}
+
+interface IBreakable {
+    int durability {get; set;}
+    void Break();
+}
+
+interface ICollectable {
+    bool isCollected {get;}
+    void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable {
+    
+}
+

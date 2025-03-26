75038e285f519ae58a5c7c32a0a3ddb9822165cf
SergioLebron22 <lebronsergio22@gmail.com>
2025-03-26 17:55:45 -0400

    refactor: update Door, Decoration, and Key classes; improve method implementations and documentation

--- csharp-interfaces/6-generic_iteration/6-generic_iteration.cs
+++ csharp-interfaces/6-generic_iteration/6-generic_iteration.cs
@@ -1,6 +1,6 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections;
-using System.Collections.Generic
+using System.Collections.Generic;
 
 /// <summary>
 /// Base class
@@ -46,55 +46,40 @@ interface ICollectable
 }
 
 /// <summary>
-/// TestObject class
+/// Door class
 /// </summary>
-class TestObject : Base, IInteractive, IBreakable, ICollectable
+class Door : Base, IInteractive
 {
-    public int durability { get; set; }
-    public bool isCollected { get; set; }
+    public Door(string value = "Door")
+    {
+        this.name = value;
+    }
 
-    /// <summary>
-    /// Interact method implementation
-    /// </summary>
     public void Interact()
     {
-        Console.WriteLine("Interacting with the object.");
-    }
-
-    /// <summary>
-    /// Break method implementation
-    /// </summary>
-    public void Break()
-    {
-        Console.WriteLine("Breaking the object.");
-    }
-
-    /// <summary>
-    /// Collect method implementation
-    /// </summary>
-    public void Collect()
-    {
-        isCollected = true;
-        Console.WriteLine("Collecting the object.");
-    }
-}
-
-class Door : Base, IInteractive {
-    public Door(string name = "Door") {
-        this.name = name;
-    }
-
-    public void Interact() {
         Console.WriteLine($"You try to open the {name}. It's locked.");
     }
 }
 
-class Decoration : Base, IInteractive, IBreakable {
-    public bool isQuestItem;
-    public int durability {get; set;}
+/// <summary>
+/// Decoration class
+/// </summary>
+class Decoration : Base, IInteractive, IBreakable
+{
+    public bool isQuestItem { get; set; }
+    public int durability { get; set; }
 
-    public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false) {
-        if (durability <= 0) {
+    /// <summary>
+    /// Decoration constructor
+    /// </summary>
+    /// <param name="name">name of the decoration</param>
+    /// <param name="durability">durability of the decoration</param>
+    /// <param name="isQuestItem">chack if is a quest item</param>
+    /// <exception cref="Exception">durabilyty needs to be more that 0</exception>
+    public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false)
+    {
+        if (durability <= 0)
+        {
             throw new Exception("Durability must be greater than 0");
         }
         this.name = name;
@@ -102,60 +87,102 @@ class Decoration : Base, IInteractive, IBreakable {
         this.isQuestItem = isQuestItem;
     }
 
-    public void Interact() {
-        if (durability <= 0) {
+    /// <summary>
+    /// Interact method
+    /// </summary>
+    public void Interact()
+    {
+        if (durability <= 0)
+        {
             Console.WriteLine($"The {name} has been broken.");
         }
-        else if (isQuestItem) {
+        else if (isQuestItem)
+        {
             Console.WriteLine($"You look at the {name}. There's a key inside.");
         }
-        else {
+        else
+        {
             Console.WriteLine($"You look at the {name}. Not much to see here.");
         }
     }
 
-    public void Break() {
+    /// <summary>
+    /// Break method
+    /// </summary>
+    public void Break()
+    {
         durability--;
-        if (durability > 0) {
+        if (durability > 0)
+        {
             Console.WriteLine($"You hit the {name}. It cracks.");
         }
-        else if (durability == 0) {
+        else if (durability == 0)
+        {
             Console.WriteLine($"You smash the {name}. What a mess.");
         }
-        else {
+        else
+        {
             Console.WriteLine($"The {name} is already broken.");
         }
     }
 }
 
-class Key : Base, ICollectable {
+/// <summary>
+/// Key class
+/// </summary>
+class Key : Base, ICollectable
+{
+    public bool isCollected { get; set; }
 
-    public bool isCollected {get; set;}
-    public Key(string name = "Key", bool isCollected = false) {
+    /// <summary>
+    /// Key constructor
+    /// </summary>
+    /// <param name="name">name of the key</param>
+    /// <param name="isCollected">check if is collected</param>
+    public Key(string name = "Key", bool isCollected = false)
+    {
         this.name = name;
         this.isCollected = isCollected;
     }
 
-    public void Collect() {
-        if (isCollected) {
-            Console.WriteLine($"You have already picked up the {name}.");
-        }
-        else {
+    /// <summary>
+    /// Collect method
+    /// </summary>
+    public void Collect()
+    {
+        if (!isCollected)
+        {
             isCollected = true;
             Console.WriteLine($"You pick up the {name}.");
+        }
+        else
+        {
+            Console.WriteLine($"You have already picked up the {name}.");
         }
     }
 }
 
+/// <summary>
+/// Objs class
+/// </summary>
+/// <typeparam name="T">type of the objs</typeparam>
 class Objs<T> : IEnumerable<T>
 {
     List<T> objs = new List<T>();
 
-
+    /// <summary>
+    /// Add method
+    /// </summary>
+    /// <param name="obj">obj to add</param>
     public void Add(T obj)
     {
         objs.Add(obj);
     }
+
+    /// <summary>
+    /// GetEnumerator method
+    /// </summary>
+    /// <returns>IEnumerator</returns>
     public IEnumerator<T> GetEnumerator()
     {
         foreach (T obj in objs)
@@ -163,9 +190,13 @@ class Objs<T> : IEnumerable<T>
             yield return obj;
         }
     }
+
+    /// <summary>
+    /// GetEnumerator method
+    /// </summary>
+    /// <returns>IEnumerator</returns>
     IEnumerator IEnumerable.GetEnumerator()
     {
         return this.GetEnumerator();
     }
 }
-

1fa98e701687f8c0b78f6c870f1d585a47dba4ac
SergioLebron22 <lebronsergio22@gmail.com>
2025-03-28 16:53:55 -0400

    feat: add Player class for damage management; implement health handling methods and project file

---
+++ csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Player {
+    private string name {get; set;}
+    private float maxHp {get; set;}
+    private float hp {get; set;}
+
+    public Player(string name = "Player", float maxHp = 100f) {
+        if (maxHp <= 0) {
+            maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+
+    public void PrintHealth() {
+        Console.Write($"{name} has {hp} / {maxHp} health\n");
+    }
+
+    public delegate void CalculateHealth(float damage);
+
+    public void TakeDamage(float damage) {
+        if (damage < 0) {
+            damage = 0;
+        }
+        Console.WriteLine($"{name} takes {damage} damage!");
+    }
+
+    public void HealDamage(float heal) {
+        if (heal < 0) {
+            heal = 0;
+        }
+        Console.WriteLine($"{name} heals {heal} HP!");
+    }
+}
